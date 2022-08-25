590ce617faba2753537510f3369d791622014289
Glenn <me@promise.solutions>
2022-08-25 08:19:41 +0000

    Refactor interactions to verify using `discord-verify` package

--- src/routes/interactionPost.ts
+++ src/routes/interactionPost.ts
@@ -1,13 +1,13 @@
 import type { APIApplicationCommandInteraction, APIPingInteraction } from "discord-api-types/v10";
 import { InteractionResponseType, InteractionType } from "discord-api-types/v10";
+import { PlatformAlgorithm, isValidRequest } from "discord-verify/web";
 import commandHandler from "../commands";
 import { respond } from "../utils/discordInteractions";
-import verifyDiscordSignature from "../utils/verifyDiscordSignature";
 
 export default async function handleInteractionPost(request: Request): Promise<Response> {
   // check and validate headers
   if (!request.headers.get("X-Signature-Ed25519") || !request.headers.get("X-Signature-Timestamp")) return Response.redirect(FALLBACK_URL);
-  if (!await verifyDiscordSignature(request)) return new Response("", { status: 401 });
+  if (!await isValidRequest(request, DISCORD_PUBLIC_KEY, PlatformAlgorithm.Cloudflare)) return new Response("", { status: 401 });
 
   // parse request body
   const interaction: APIApplicationCommandInteraction | APIPingInteraction = await request.json();
