7b659bb73f1582e6bf47b955e78654547798551d
Promise <me@promise.solutions>
2025-07-16 12:46:07 +0000

    chore: tidy things up and build application from cloudflare
    Signed-off-by: Promise <me@promise.solutions>

--- src/utils/twilio.ts
+++ src/utils/twilio.ts
@@ -5,23 +5,27 @@ const token = btoa(`${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}`);
 export default async function makeCall(message: string, statusCallback: URL): Promise<CallInstance> {
   const endpoint = `https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Calls.json`;
 
-  const headers = new Headers();
-  headers.set("Authorization", `Basic ${token}`);
-  headers.set("Content-Type", "application/x-www-form-urlencoded");
-
-  const body = new URLSearchParams();
-  body.append("To", MY_PHONE_NUMBER);
-  body.append("From", TWILIO_PHONE_NUMBER);
-  body.append("Twiml", `<Response><Say>${
-    Array<string>(3)
+  const encoded = new URLSearchParams({
+    To: MY_PHONE_NUMBER,
+    From: TWILIO_PHONE_NUMBER,
+    Twiml: `<Response><Say>${Array<string>(3)
       .fill(message)
       .join("</Say><Pause length=\"3\"/><Say>")
-  }</Say></Response>`);
-  body.append("StatusCallback", statusCallback.toString());
-  ["initiated", "ringing", "answered", "completed"].forEach(status => {
-    body.append("StatusCallbackEvent", status);
+    }</Say></Response>`,
+    StatusCallback: statusCallback.toString(),
   });
 
-  const response = await fetch(endpoint, { method: "POST", headers, body });
+  ["initiated", "ringing", "answered", "completed"].forEach(status => {
+    encoded.append("StatusCallbackEvent", status);
+  });
+
+  const response = await fetch(endpoint, {
+    body: encoded,
+    method: "POST",
+    headers: {
+      "Authorization": `Basic ${token}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
   return response.json();
 }
