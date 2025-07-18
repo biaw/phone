86d642c261701cf46965b4e7c90788904251475a
Promise <me@promise.solutions>
2025-07-18 17:46:13 +0000

    fix: migrate to service worker
    ref. https://developers.cloudflare.com/workers/reference/migrate-to-module-workers/#migrate-a-worker
    
    Signed-off-by: Promise <me@promise.solutions>

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,44 @@ import handleInteractionPost from "./routes/interactionPost";
 import sendInvite from "./routes/invite";
 import handleUpdateCommands from "./routes/updateCommands";
 
-addEventListener("fetch", (event: FetchEvent) => {
-  // test environment
-  try {
-    /* eslint-disable @typescript-eslint/no-unused-expressions */
-    DISCORD_ID;
-    DISCORD_PUBLIC_KEY;
-    DISCORD_TOKEN;
-    TWILIO_ACCOUNT_SID;
-    TWILIO_AUTH_TOKEN;
-    TWILIO_PHONE_NUMBER;
-    MY_PHONE_NUMBER;
-    FALLBACK_URL;
-    /* eslint-enable @typescript-eslint/no-unused-expressions */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (err) {
-    return event.respondWith(logResponse(new Response("Invalid environment variables", { status: 500 })));
-  }
+export default {
+  fetch(request) {
+    // test environment
+    try {
+      /* eslint-disable @typescript-eslint/no-unused-expressions */
+      DISCORD_ID;
+      DISCORD_PUBLIC_KEY;
+      DISCORD_TOKEN;
+      TWILIO_ACCOUNT_SID;
+      TWILIO_AUTH_TOKEN;
+      TWILIO_PHONE_NUMBER;
+      MY_PHONE_NUMBER;
+      FALLBACK_URL;
+      /* eslint-enable @typescript-eslint/no-unused-expressions */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log("Invalid environment variables");
+      return new Response("Invalid environment variables", { status: 500 });
+    }
 
-  const { method } = event.request;
-  const url = new URL(event.request.url);
-
-  // routes for the owner
-  if (method === "GET" && url.pathname === "/update-commands") return event.respondWith(handleUpdateCommands(event.request).then(logResponse));
-  if (method === "GET" && url.pathname === "/invite") return event.respondWith(logResponse(sendInvite()));
-
-  // routes for APIs
-  if (method === "POST" && url.pathname === "/call-updates") return event.respondWith(handleCallStatusUpdate(event.request).then(logResponse));
-  if (method === "POST" && url.pathname === "/interaction") return event.respondWith(handleInteractionPost(event.request).then(logResponse));
-
-  return event.respondWith(logResponse(Response.redirect(FALLBACK_URL, 307)));
-});
+    switch (request.method) {
+      case "GET":
+        switch (new URL(request.url).pathname) {
+          case "/update-commands": return handleUpdateCommands(request).then(logResponse);
+          case "/invite": return logResponse(sendInvite());
+          default: return logResponse(Response.redirect(FALLBACK_URL, 307));
+        }
+      case "POST":
+        switch (new URL(request.url).pathname) {
+          case "/call-updates": return handleCallStatusUpdate(request).then(logResponse);
+          case "/interaction": return handleInteractionPost(request).then(logResponse);
+          default: return logResponse(new Response("Method Not Allowed", { status: 405 }));
+        }
+      default: return logResponse(new Response("Method Not Allowed", { status: 405 }));
+    }
+  },
+} satisfies ExportedHandler;
 
 function logResponse(response: Response): Response {
   // eslint-disable-next-line no-console
